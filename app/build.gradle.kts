315695a07bd5e64ddf8c0c266eafc65c360cc1d4
WanQi-01 <wgoh0026@student.monash.edu>
2026-02-22 02:04:22 +0800

    Rename photo, add MainActivity file, change theme to AppCompat
    - add implementation of AppCompat to build.gradle.kts file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/mediai/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.mediai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Set;
+
+import java.util.HashSet;
+/**
+ * Represents the main class for running the app.
+ */
+public class MainActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle instance) {
+        super.onCreate(instance);
+
+    }
+
+}
