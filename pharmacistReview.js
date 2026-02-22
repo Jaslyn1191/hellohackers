8feb80c36aa85b991f10651fabffb6c172085b20
Stella Wong Kai Ning <swon0163@student.monash.edu>
2026-02-22 23:07:38 +0800

    Initialise pharmacistReview.json

---
+++ pharmacistReview.js
@@ -0,0 +1,14 @@
+import db from "./firebase-admin.js";
+
+export async function getUserPendingCases() {
+  const cases = await db.collection("cases")
+    .where("status", "Pending")
+    .get();
+
+  const pendingCases = [];
+  cases.forEach(doc => {
+    pendingCases.push({ caseId: doc.id });
+  });
+
+  return pendingCases;
+}
